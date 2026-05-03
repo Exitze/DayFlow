a0dd61dbb83b41b19a4ee852e3320295c25c9d20
Exitze <Exitze@icloud.com>
2026-05-03 12:51:09 +0700

    Show notifications while app is open

--- Dayflow/DayflowApp.swift
+++ Dayflow/DayflowApp.swift
@@ -1,10 +1,32 @@
 import SwiftUI
+import UIKit
+import UserNotifications
 
 @main
 struct DayflowApp: App {
+    @UIApplicationDelegateAdaptor(DayflowAppDelegate.self) private var appDelegate
+
     var body: some Scene {
         WindowGroup {
             DayflowHomeView()
         }
+    }
+}
+
+final class DayflowAppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        UNUserNotificationCenter.current().delegate = self
+        return true
+    }
+
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([.banner, .list, .sound, .badge])
     }
 }
